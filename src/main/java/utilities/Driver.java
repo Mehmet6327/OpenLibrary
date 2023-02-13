713b9ba66b9e4f9b40fc1d4108d12f7918aacbd3
Mehmet Buyukertas <73278559+Mehmet6327@users.noreply.github.com>
2023-02-13 10:46:46 +0100

    Update Driver.java

--- src/main/java/utilities/Driver.java
+++ src/main/java/utilities/Driver.java
@@ -1,8 +1,10 @@
 package utilities;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Driver {
     private Driver() {
     }
