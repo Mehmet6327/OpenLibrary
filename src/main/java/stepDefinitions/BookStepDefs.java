1b2c1b5d0543f8b73572096d2bcff07fa5e46348
Mehmet Buyukertas <73278559+Mehmet6327@users.noreply.github.com>
2023-02-13 10:45:27 +0100

    Update BookStepDefs.java

--- src/main/java/stepDefinitions/BookStepDefs.java
+++ src/main/java/stepDefinitions/BookStepDefs.java
@@ -1,2 +1,37 @@
-package stepDefinitions;public class BookStepDefs {
+package stepDefinitions;
+
+import api.GetMethod;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.JavascriptExecutor;
+import pages.BookPage;
+import utilities.Driver;
+public class BookStepDefs {
+
+    BookPage bookPage = new BookPage();
+    String authorNameFromApi;
+    String authorNameOnBook;
+    @When("user searches for {string}")
+    public void user_searches_for(String expectedBook)  {
+        bookPage.searchBox.sendKeys(expectedBook);
+        bookPage.searchButton.click();
+    }
+    @When("user chooses book published in {string}")
+    public void user_chooses_book_published_in(String string)  {
+        JavascriptExecutor jse = (JavascriptExecutor) Driver.get();
+        jse.executeScript("arguments[0].scrollIntoView();", bookPage.publishYear);
+        authorNameOnBook = bookPage.authorName.getText();
+        bookPage.publishYear.click();
+    }
+    @And("get author from API with Endpoint {string}")
+    public void getAuthorFromAPIWithEndpoint(String endPoint) {
+        GetMethod get = new GetMethod();
+        authorNameFromApi = get.getAuthorName(endPoint);
+    }
+    @Then("author from API matches author on book page")
+    public void authorFromAPIMatchesAuthorOnBookPage()  {
+        Assert.assertEquals(authorNameOnBook,authorNameFromApi);
+    }
 }
