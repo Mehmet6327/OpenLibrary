06b6f1c841c5cb8f4ceaa5934d3a674db4eb443d
Mehmet Buyukertas <73278559+Mehmet6327@users.noreply.github.com>
2023-02-13 10:46:02 +0100

    Update LibraryStepDefs.java

--- src/main/java/stepDefinitions/LibraryStepDefs.java
+++ src/main/java/stepDefinitions/LibraryStepDefs.java
@@ -1,2 +1,25 @@
-package stepDefinitions;public class LibraryStepDefs {
-}
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import org.junit.Assert;
+import pages.LibraryMainPage;
+import utilities.ConfigurationReader;
+import utilities.Driver;
+import static org.junit.Assert.assertEquals;
+public class LibraryStepDefs {
+    @Given("user goes to the OpenLibrary page")
+    public void user_goes_to_the_OpenLibrary_page() {
+        Driver.get().get(ConfigurationReader.get("url"));
+        String expectedUrl = ConfigurationReader.get("url");
+        String actualUrl = Driver.get().getCurrentUrl();
+        assertEquals(expectedUrl,actualUrl);
+    }
+    @Given("user sets website in English")
+    public void user_sets_website_in_English()  {
+        LibraryMainPage libraryMainPage = new LibraryMainPage();
+        libraryMainPage.changeLanguage.click();
+        libraryMainPage.englishIdioma.click();
+        Assert.assertTrue(libraryMainPage.lang.getAttribute("lang").equals("en"));
+    }
+    }
+
