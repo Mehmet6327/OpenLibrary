0ee560059de462dfd37efe44a0d0c1f9259fd2c0
Mehmet Buyukertas <73278559+Mehmet6327@users.noreply.github.com>
2023-02-13 10:43:57 +0100

    Update BookPage.java

--- src/main/java/pages/BookPage.java
+++ src/main/java/pages/BookPage.java
@@ -1,2 +1,15 @@
-package pages;public class BookPage {
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class BookPage extends BasePage{
+    @FindBy(xpath = "//form[@class='search-bar-input']//input[1]")
+    public WebElement searchBox;
+    @FindBy(className = "search-bar-submit")
+    public WebElement searchButton;
+    @FindBy(xpath = "(//span[@class='bookcover']//img)[2]")
+    public WebElement publishYear;
+    @FindBy(xpath = "(//span[@class='bookauthor']//a)[2]")
+    public WebElement authorName;
 }
