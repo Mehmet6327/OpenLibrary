6a88bb6af86c82805ebdceef75a9f321358a9b21
Mehmet Buyukertas <73278559+Mehmet6327@users.noreply.github.com>
2023-02-13 10:43:02 +0100

    Update GetMethod.java

--- src/main/java/api/GetMethod.java
+++ src/main/java/api/GetMethod.java
@@ -1,2 +1,27 @@
-package api;public class GetMethod {
+package api;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import static io.restassured.RestAssured.*;
+
+public class GetMethod {
+
+    public Response callGETMethod(String endpoint){
+
+        baseURI = "https://openlibrary.org";
+
+        Response response = given().accept(ContentType.JSON).get(endpoint);
+        Assert.assertEquals(response.statusCode(),200);
+        Assert.assertEquals(response.contentType(),"application/json");
+        return response;
+    }
+
+    public String getAuthorKey(String enpoint){
+        return callGETMethod(enpoint).path("docs[0].seed[4]").toString();
+    }
+    public String getAuthorName(String enpoint){
+        return callGETMethod(getAuthorKey(enpoint)).path("name").toString();
+    }
+
 }
