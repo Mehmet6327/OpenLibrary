c615a57c4b0783094891437ff6e2fcb38e324d52
Mehmet Buyukertas <73278559+Mehmet6327@users.noreply.github.com>
2023-02-13 10:44:24 +0100

    Update LibraryMainPage.java

--- src/main/java/pages/LibraryMainPage.java
+++ src/main/java/pages/LibraryMainPage.java
@@ -1,2 +1,13 @@
-package pages;public class LibraryMainPage {
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LibraryMainPage extends BasePage {
+    @FindBy(xpath = "//div[@class='language-component header-dropdown']//summary[1]")
+    public WebElement changeLanguage;
+    @FindBy(xpath = "(//ul[@class='locale-options dropdown-menu']//a)[3]")
+    public WebElement englishIdioma;
+    @FindBy(tagName = "html")
+    public WebElement lang;
 }
